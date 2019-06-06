3d5eeb0fb0f5ff8655c380befc6caaea5854d83f
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-06 13:51:30 +0200

    understand tuple

---
+++ tuples.py
@@ -0,0 +1,7 @@
+
+#tuples is familiar to list
+#tuple we can't change and modify (tuple is immutable)
+#the difference between a list and a tuple is brackets.
+#we can make tuples in list
+coordinates = [(4, 5), (9, 12), (32, 15)]
+print(coordinates[0])
